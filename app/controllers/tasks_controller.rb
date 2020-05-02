f21ae17facf15e8c67bf31b029edd85d383eb2e8
gaogao1230 <kurukuru1230@gmial.com>
2020-05-02 11:07:34 +0000

    kadai

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
+    
+    before_action :set_task, only: [:show,:edit,:update,:destroy]
+    
     def index
         @tasks = Task.all
     end
     
     def show
-        @task = Task.find(params[:id])
-    end
+    end 
     
     def create
         @task = Task.new(task_params)
@@ -19,7 +21,6 @@ class TasksController < ApplicationController
     end 
     
     def update
-        @task = Task.find(params[:id])
         if @task.update(task_params)
             flash[:success]="正常に更新されました"
             redirect_to task_path(@task)
@@ -30,14 +31,12 @@ class TasksController < ApplicationController
     end 
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         flash[:success] = "タスクは削除されました"
         redirect_to tasks_url
     end 
     
     def edit
-        @task = Task.find(params[:id])
     end 
     
     def new
@@ -46,7 +45,11 @@ class TasksController < ApplicationController
     
     private
     
+    def set_task
+        @task = Task.find(params[:id])
+    end
+    
     def task_params
-        params.require(:task).permit(:content)
+        params.require(:task).permit(:content,:status)
     end
 end
